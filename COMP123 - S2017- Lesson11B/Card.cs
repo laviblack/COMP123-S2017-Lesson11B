4ba34b3a7a36f982fdf2100cb11af73efa030695
Lyrica Yoshida <lyoshida@my.centennialcollege.ca>
2017-07-25 17:19:49 -0400

    Added the Card class

---
+++ COMP123 - S2017- Lesson11B/Card.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * Name: Lyrica Yoshida
+ * ID: 300923951
+ * Date: July 25, 2017
+ * Description: This is the Card class
+ * Version: 0.1 - Created the Card class
+ */
+
+namespace COMP123___S2017__Lesson11B
+{
+    class Card
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private Face _face;
+        private Suit _suit;
+
+        // PUBLIC PROPERTIES
+        public Face Face {
+            get
+            {
+                return this._face;
+            }
+            set
+            {
+                this._face = value;
+            }
+        }
+
+        public Suit Suit
+        {
+            get
+            {
+                return this._suit;
+            }
+            set
+            {
+                this._suit = value;
+            }
+        }
+        
+        // CONSTRUCTOR
+
+        /// <summary>
+        /// This is the main constructor for the Card class
+        /// It take twe parameters - face (Face) - suit (Suit)
+        /// </summary>
+        /// <param name="face"></param>
+        /// <param name="suit"></param>
+        public Card(Face face, Suit suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+
+    }
+}
