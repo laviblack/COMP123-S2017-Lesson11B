824c0cc0a4bf75b2dc838a5bba4d397b24398962
Lyrica Yoshida <lyoshida@my.centennialcollege.ca>
2017-07-27 17:04:45 -0400

    Tested the Deal1 method of the Deck class in the driver class

--- COMP123 - S2017- Lesson11B/Program.cs
+++ COMP123 - S2017- Lesson11B/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
  * ID: 300923951
  * Date: July 25, 2017
  * Description: This is a demo for Lesson 11
- * Version: 0.3 - Tested the Shuffle method of the Deck class
+ * Version: 0.4 - Tested the Deal1 method of the Deck class
  */
 
 namespace COMP123___S2017__Lesson11B
@@ -19,10 +19,20 @@ namespace COMP123___S2017__Lesson11B
         static void Main(string[] args)
         {
             Deck deck = new Deck();
+            Hand hand = new Hand();
             Console.WriteLine(deck.ToString());
             Console.WriteLine();
             deck.Shuffle();
             Console.WriteLine("Shuffled");
+            Console.WriteLine(deck.ToString());
+
+            hand.Add(deck.Deal1());
+            Console.WriteLine(hand.ToString());
+            Console.WriteLine();
+            hand.Add(deck.Deal1());
+            Console.WriteLine(hand.ToString());
+
+            Console.WriteLine();
             Console.WriteLine(deck.ToString());
         }
     }
